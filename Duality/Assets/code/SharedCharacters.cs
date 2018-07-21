1952c56492beb42e6ed9bb24f9229d5fdcf65313
ashnh <ashnhx@gmail.com>
2018-07-21 15:36:25 -0400

    starting repository

---
+++ Duality/Assets/code/SharedCharacters.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharedCharacters : MonoBehaviour {
+
+	public GameObject red;
+	public GameObject blue;
+
+	public GameObject cameraObject;
+
+	public float speed;
+
+	bool isControllingBlue;
+
+	void Start () {
+
+		isControllingBlue = true;
+
+	}
+	
+	void Update () {
+
+		GameObject currentlyControlled = (isControllingBlue) ? blue : red;
+		GameObject currentlyUncontrolled = (!isControllingBlue) ? blue : red;
+
+		if (Input.GetKey (KeyCode.W)) {
+			currentlyControlled.GetComponent <Rigidbody2D> ().velocity = new Vector2 (currentlyControlled.GetComponent <Rigidbody2D> ().velocity.x, speed);
+		} else if (Input.GetKey (KeyCode.S)) {
+			currentlyControlled.GetComponent <Rigidbody2D> ().velocity = new Vector2 (currentlyControlled.GetComponent <Rigidbody2D> ().velocity.x, -speed);
+		} else {
+			currentlyControlled.GetComponent <Rigidbody2D> ().velocity = new Vector2 (currentlyControlled.GetComponent <Rigidbody2D> ().velocity.x, 0);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			currentlyControlled.GetComponent <Rigidbody2D> ().velocity = new Vector2 (speed, currentlyControlled.GetComponent <Rigidbody2D> ().velocity.y);
+		} else if (Input.GetKey (KeyCode.A)) {
+			currentlyControlled.GetComponent <Rigidbody2D> ().velocity = new Vector2 (-speed, currentlyControlled.GetComponent <Rigidbody2D> ().velocity.y);
+		} else {
+			currentlyControlled.GetComponent <Rigidbody2D> ().velocity = new Vector2 (0, currentlyControlled.GetComponent <Rigidbody2D> ().velocity.y);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space))
+			isControllingBlue = !isControllingBlue;
+
+		currentlyUncontrolled.GetComponent <Rigidbody2D> ().velocity = new Vector2 (0, 0);
+
+		cameraObject.transform.position = new Vector3 (currentlyControlled.transform.position.x, currentlyControlled.transform.position.y, -10);
+
+	}
+
+	public bool getIsControllingBlue () {
+
+		return isControllingBlue;
+
+	}
+}
---
+++ Duality/Assets/code/Subordinate.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subordinate : MonoBehaviour {
+
+	public bool blue;
+
+	public float speed;
+	public float slowDownDistance;
+	public float controlDistance;
+
+	public SharedCharacters sc;
+
+	public GameObject redObject;
+	public GameObject blueObject;
+
+	public Camera cameraObject;
+
+	// Use this for initialization
+	void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Mouse0) && (sc.getIsControllingBlue() == blue)) {
+
+			GameObject controlObject = (blue) ? blueObject : redObject;
+
+			if (Mathf.Sqrt (Mathf.Pow (controlObject.transform.position.x - transform.position.x, 2) + Mathf.Pow (controlObject.transform.position.y - transform.position.y, 2)) <= controlDistance)
+				GetComponent <Rigidbody2D> ().velocity = getMovement ();
+			else
+				GetComponent <Rigidbody2D> ().velocity = new Vector2 (0, 0);
+
+		} else {
+
+			GetComponent <Rigidbody2D> ().velocity = new Vector2 (0, 0);
+
+		}
+
+	}
+
+	Vector2 getMovement () {
+
+
+		Vector3 mousePosInSpace = Camera.main.ScreenToWorldPoint( new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f) );
+
+		Vector2 rawDifference = new Vector2 (mousePosInSpace.x - transform.position.x, mousePosInSpace.y - transform.position.y);
+
+		float speedModifier = Mathf.Sqrt (Mathf.Pow (mousePosInSpace.x - transform.position.x, 2) + Mathf.Pow (mousePosInSpace.y - transform.position.y, 2)) / slowDownDistance;
+
+		if (speedModifier > 1f)
+			speedModifier = 1f;
+
+		float biggerFloat = (Mathf.Abs(rawDifference.x) > Mathf.Abs(rawDifference.y)) ? Mathf.Abs(rawDifference.x) : Mathf.Abs(rawDifference.y);
+
+		return new Vector2 (rawDifference.x / biggerFloat * speed * speedModifier, rawDifference.y / biggerFloat * speed * speedModifier);
+
+	}
+}
