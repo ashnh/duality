4cbfea946f7f4fb0147c36f9c3e6043f0b58c8a0
ashnh <ashnhx@gmail.com>
2018-07-23 21:01:04 -0400

    added global gehaviors

--- Duality/Assets/code/Subordinate.cs
+++ Duality/Assets/code/Subordinate.cs
@@ -17,25 +17,69 @@ public class Subordinate : MonoBehaviour {
 
 	public Camera cameraObject;
 
+	bool awakened;
+	bool massMovement;
+
+	bool fightOrdered;
+
 	// Use this for initialization
 	void Start () {
 		
-
+		awakened = false;
+		massMovement = false;
+		fightOrdered = false;
 
 	}
-	
+
+	bool playerInCommandPosition (GameObject controlObject) {
+		return Mathf.Sqrt (Mathf.Pow (controlObject.transform.position.x - transform.position.x, 2)
+		+ Mathf.Pow (controlObject.transform.position.y - transform.position.y, 2)) <= controlDistance;
+	}
+		
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag != "subordinate")
+			massMovement = false;
+	}
+
 	// Update is called once per frame
 	void Update () {
+
+		GameObject controlObject = (blue) ? blueObject : redObject;
+
 		if (Input.GetKey (KeyCode.Mouse0) && (sc.getIsControllingBlue() == blue)) {
 
-			GameObject controlObject = (blue) ? blueObject : redObject;
+			//////////shift to be fight active key
+			if (Input.GetKey (KeyCode.Q) && awakened) {
+				massMovement = true;
 
-			if (Mathf.Sqrt (Mathf.Pow (controlObject.transform.position.x - transform.position.x, 2) + Mathf.Pow (controlObject.transform.position.y - transform.position.y, 2)) <= controlDistance)
+				//////////idea: make blue slow down but not red
+				if (blue) {
+					Vector2 movement = evenDirectionalMovement (transform.position, controlObject.transform.position);
+					float speedModifier = getSpeedModifier (transform.position, controlObject.transform.position);
+
+					GetComponent <Rigidbody2D> ().velocity = new Vector2 (movement.x * speedModifier * speed,
+						movement.y * speedModifier * speed);
+
+				} else {
+
+					Vector3 mousePosInSpace = Camera.main.ScreenToWorldPoint(
+						new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f) );
+
+					Vector2 movement = evenDirectionalMovement (controlObject.transform.position, mousePosInSpace);
+
+					GetComponent <Rigidbody2D> ().velocity = new Vector2 (movement.x * speed, movement.y * speed);
+
+				}
+
+				awakened = false;
+
+			} else if (playerInCommandPosition (controlObject)) {
+				awakened = true;
+				massMovement = false;
 				GetComponent <Rigidbody2D> ().velocity = getMovement ();
-			else
-				GetComponent <Rigidbody2D> ().velocity = new Vector2 (0, 0);
+			}
 
-		} else {
+		} else if (!massMovement) {
 
 			GetComponent <Rigidbody2D> ().velocity = new Vector2 (0, 0);
 
@@ -48,16 +92,36 @@ public class Subordinate : MonoBehaviour {
 
 		Vector3 mousePosInSpace = Camera.main.ScreenToWorldPoint( new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f) );
 
-		Vector2 rawDifference = new Vector2 (mousePosInSpace.x - transform.position.x, mousePosInSpace.y - transform.position.y);
+		Vector2 moveVector = evenDirectionalMovement (transform.position, mousePosInSpace);
 
-		float speedModifier = Mathf.Sqrt (Mathf.Pow (mousePosInSpace.x - transform.position.x, 2) + Mathf.Pow (mousePosInSpace.y - transform.position.y, 2)) / slowDownDistance;
+		float speedModifier = getSpeedModifier (transform.position, mousePosInSpace);
+
+		return new Vector2 (moveVector.x * speed * speedModifier, moveVector.y * speed * speedModifier);
+
+	}
+
+	float getSpeedModifier (Vector3 placeFrom, Vector3 placeTo) {
+
+		float speedModifier = Mathf.Sqrt (Mathf.Pow (placeTo.x - placeFrom.x, 2)
+			+ Mathf.Pow (placeTo.y - placeFrom.y, 2)) / slowDownDistance;
 
 		if (speedModifier > 1f)
 			speedModifier = 1f;
 
-		float biggerFloat = (Mathf.Abs(rawDifference.x) > Mathf.Abs(rawDifference.y)) ? Mathf.Abs(rawDifference.x) : Mathf.Abs(rawDifference.y);
-
-		return new Vector2 (rawDifference.x / biggerFloat * speed * speedModifier, rawDifference.y / biggerFloat * speed * speedModifier);
+		return speedModifier;
 
 	}
+
+	Vector2 evenDirectionalMovement (Vector3 placeFrom, Vector3 placeTo) {
+
+		Vector2 rawDifference = new Vector2 (placeTo.x - placeFrom.x, placeTo.y - placeFrom.y);
+
+		float biggerFloat = (Mathf.Abs(rawDifference.x) > Mathf.Abs(rawDifference.y))
+			? Mathf.Abs(rawDifference.x) : Mathf.Abs(rawDifference.y);
+
+		return new Vector2 (rawDifference.x / biggerFloat, rawDifference.y / biggerFloat);
+
+	}
+
+
 }
